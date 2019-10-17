d16804e904b62d5de9985189200fdcca7029756f
anrystate86 <anry_state86@mail.ru>
2019-10-17 20:43:30 +0700

    adding script files

---
+++ APIFunc.ps1
@@ -0,0 +1,140 @@
+﻿# Get User from API token
+function GetAPIUser ($APIToken)
+{
+    $TReq = "/v0.1/user"
+    $TMethod = "GET"
+    (InvokeAPI -Req $TReq -Method $TMethod -APIToken $APIToken).name
+}
+
+# Test API token for later authorization
+function TestAPIKey ($APIToken)
+{
+    
+    $Resp = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/user" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"="$($APIToken)"; "Content-Type"="application/json"} -WarningAction SilentlyContinue;
+    If ($Resp.StatusCode -eq 200)
+    {
+        Return $true
+    }
+    elseif ($Resp.StatusCode -eq 401)
+    {
+        Write-Host "Error: (401) Unauthorized" -f Red
+        Return $false
+    }
+    else
+    {
+        Write-Host $Error[0] -f Red
+        Return $false
+    }
+}
+
+# Get Applications
+function GetAPIApps ($APIToken)
+{
+    $TReq = "/v0.1/apps"
+    $TMethod = "GET"
+    (InvokeAPI -Req $TReq -Method $TMethod -APIToken $APIToken).name
+}
+
+# Get Branches from selected Application
+function GetAPIBranches ($APPName, $APIToken)
+{
+    $TReq = "/v0.1/apps/$(GetAPIUser -APIToken $APIToken)/$APPName/branches"
+    $TMethod = "GET"
+    (InvokeAPI -Req $TReq -Method $TMethod -APIToken $APIToken)
+    #name, commit, protected, protection, protection_url
+}
+
+# Invoke API web request with $Req param, $Method and $APIToken
+function InvokeAPI ($Req, $Method, $APIToken)
+{
+    $Resp = Invoke-WebRequest -Uri "https://api.appcenter.ms$Req" -Method $Method -Headers @{"Accept"="application/json"; "X-API-Token"="$($APIToken)"; "Content-Type"="application/json"};
+    If ($Resp.StatusCode -eq 200)
+    {
+        Return $(ConvertFrom-Json $($Resp.Content))
+    }
+    else
+    {
+        Write-Host $Resp.StatusDescription -ForegroundColor Red
+        exit
+    }
+}
+
+# Start build selected Application/Branch 
+function StartBuild ($APPName, $APIToken, $Branch)
+{
+    $Request = "/v0.1/apps/$(GetAPIUser -APIToken $APIToken)/$($APPName)/branches/$($Branch)/builds"
+    $Method = "POST"
+    InvokeAPI -Req $Request -Method $Method -APIToken $APIToken | select @{N='BranchName';E={$_.sourceBranch}}, @{N='BuildID';E={$_.id}}
+    
+    <#
+    id              : 5
+    buildNumber     : 5
+    queueTime       : 2019-10-16T12:35:59.8029452Z
+    lastChangedDate : 2019-10-16T12:35:59.86Z
+    status          : notStarted
+    reason          : manual
+    sourceBranch    : master
+    tags            : {}
+    properties      : 
+    #>
+}
+
+# Get build config for selected Application/Branch
+function GetBranchConfig ($APPName, $APIToken, $Branch)
+{
+    #/v0.1/apps/{owner_name}/{app_name}/branches/{branch}/config
+    try
+    {
+        $Resp = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$(GetAPIUser -APIToken $APIToken)/$($APPName)/branches/$($Branch)/config" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"="$($APIToken)"; "Content-Type"="application/json"} -ErrorAction SilentlyContinue -WarningAction SilentlyContinue;
+        Return $(ConvertFrom-Json $($Resp.Content))
+    }
+    catch [System.Net.WebException]
+    {
+        if ($_.exception -match "(404)")
+        {
+            Return "Not Found"
+        }
+        else
+        {
+            Write-Host "Something goes wrong:" -f Red
+            Write-Host $_.exception -f Red
+            break
+        }
+    }
+}
+
+# Get last build status for Application/Branch
+function GetBuildStatus ($APPName, $APIToken, $BuildID)
+{
+    #/v0.1/apps/{owner_name}/{app_name}/builds/{build_id}
+    $Request = "/v0.1/apps/$(GetAPIUser -APIToken $APIToken)/$($APPName)/builds/$($BuildID)"
+    $Method = "POST"
+    InvokeAPI -Req $Request -Method $Method -APIToken $APIToken | select id, startTime, finishTime, status, result
+
+    <#
+    id              : 6
+    buildNumber     : 6
+    queueTime       : 2019-10-16T12:41:39.8121022Z
+    startTime       : 2019-10-16T12:41:47.4042167Z
+    finishTime      : 2019-10-16T12:42:22.7477444Z
+    lastChangedDate : 2019-10-16T12:42:22.833Z
+    status          : completed
+    result          : failed
+    reason          : manual
+    sourceBranch    : branch1
+    sourceVersion   : 24cd8ef69079e255ad67ee7e1ff9d2e7e529e064
+    tags            : {normal, signed, uwp, manual}
+    properties      : 
+    #>
+}
+
+# Get download link for logs selected Application/BuildID
+function GetBuildLogs ($APPName, $APIToken, $BuildID)
+{
+    #/v0.1/apps/{owner_name}/{app_name}/builds/{build_id}/downloads/{download_type}
+    $Request = "/v0.1/apps/$(GetAPIUser -APIToken $APIToken)/$($APPName)/builds/$($BuildID)/downloads/logs"
+    $Method = "GET"
+    InvokeAPI -Req $Request -Method $Method -APIToken $APIToken 
+
+    #uri : https://build.appcenter.ms/v0.1/public/apps/595e04de-59bb-4759-841b-ad386e2483a0/downloads?token=97bfca897c8fcc02de9b35ce50b47f8f733dbf44d7701cc671909025e63599e7
+}
---
+++ BuildAppCenterv1.ps1
@@ -0,0 +1,216 @@
+﻿# Using APIFunc.ps1
+. $PSScriptRoot\APIFunc.ps1
+
+##########################################################
+Write-Host "AKVELON test AppCenter API PSScript" -f Green
+Write-Host
+
+$APIToken = Read-Host -Prompt "Insert API key"
+Write-Host
+
+if (TestAPIKey -APIToken $APIToken)
+{
+    Write-Host "Token User: $(GetAPIUser -APIToken $APIToken)" -ForegroundColor Green
+    
+    ##################### Applications list #######################
+    Write-Host "Applications:"
+    $Apps = GetAPIApps -APIToken $APIToken
+    if ($Apps -ne $nul)
+    {
+        $Apps
+    }
+    else
+    {
+        Write-Host "none"
+        Write-Host "There is no applications, script will be stopped"
+        break
+    }
+    Write-Host
+    
+    $Application = Read-Host "Insert apllication name for branch list"
+    
+    ################### Branches list #########################
+    Write-Host
+    Write-Host "Branches:"
+    $Branches = (GetAPIBranches -APPName $Application -APIToken $APIToken).branch.name
+    if ($Branches -ne $null)
+    {
+        $Branches   
+    }
+    else
+    {
+        Write-Host "none"
+        Write-Host "There is no branches in $($Application), script will be stopped"
+        break
+    }
+    ################## What to do whith branches ########################
+    $Wtd = 0
+    while ($Wtd -ne 5)
+    {
+        Write-Host
+        Write-Host "Select next step:"
+        Write-Host "1 Build all branches"
+        Write-Host "2 Get branches last build"
+        Write-Host "3 Get status report"
+        Write-Host "4 Save status report to HTML"
+        Write-Host "5 Exit script"
+        $Wtd = Read-Host 
+    
+        switch ($Wtd)
+        {
+            ###################### Build all branches #############################
+            1 {  
+                $Config = (GetAPIBranches -APPName $Application -APIToken $APIToken).branch.name | foreach {
+                    $BConf = GetBranchConfig -APPName $Application -APIToken $APIToken -Branch $_
+                    if (($Bconf -ne "Not found") -and ($Bconf -ne $Null))
+                    {
+                        $BrStatus = ''|select Name, Configured
+                        $BrStatus.Name = $_
+                        $BrStatus.Configured = $True
+                        $BrStatus
+                    }
+                    else
+                    {
+                        Write-host "Branch " -NoNewline
+                        Write-Host $_ -f Yellow -NoNewline
+                        Write-Host " has no Build configuration"
+
+                        $BrStatus = ''|select Name, Configured
+                        $BrStatus.Name = $_
+                        $BrStatus.Configured = $False
+                        $BrStatus
+                    }
+                }
+                $Config
+
+                If (($Config.Configured -contains $False) -and ($Config.Configured -contains $True))
+                {
+                    Write-Host "Branch(es): $($Config | where {$_.Coonfigured -eq $false} | select Name) is not configured, please configure it before Build" -f Yellow
+                    Write-Host 
+                    $Answ = Read-Host "Complete build with configured branch(es)? (y,n)" 
+                    switch ($Answ)
+                    {
+                        {"Y" -or "y"} {
+                            $Cbranches = ($Config | where {$_.Configured -eq $True}).Name
+                            $StartedBuilds = $Cbranches | foreach {
+                                StartBuild -APPName $Application -APIToken $APIToken -Branch $_
+                            }
+                            Write-Host
+                            Write-Host "Started build branches" -f Green
+                            $StartedBuilds | Format-Table
+                        }
+                        {"N" -or "n"} {Write-Host "Build was canceled" -f Yellow}
+                        DEFAULT { Write-Host "Sorry wrong answer, build was canceled" -f Yellow}
+
+                    }
+                }
+                elseif ($Config.Configured -notcontains $False)
+                {
+                    Write-Host "All branches configured"
+                    $StartedBuilds = $Config.Name | foreach {
+                        StartBuild -APPName $Application -APIToken $APIToken -Branch $_
+                    }
+                    Write-Host
+                    Write-Host "Started build branches" -f Green
+                    $StartedBuilds | Format-Table
+                }
+                else
+                {
+                    Write-Host "Sorry, there is no configured branch" -f Yellow
+                }
+
+            }
+            ################################# Branches last build ##########################
+            2 { 
+                $BrLastBuild = (GetAPIBranches -APPName $Application -APIToken $APIToken).lastbuild | select sourceBranch, id, status, startTime, finishTime
+        
+                $LBuildStats = $BrLastBuild|foreach {
+                        $Blb = "" | select BranchName, BuildID, Status, Duration
+                        $Blb.BranchName = $_.sourceBranch
+                        $Blb.BuildID = $_.id
+                        $Blb.Status = $_.status
+                        if ($Blb.Status -eq "completed")
+                        {
+                            $Duration = ($([datetime]::Parse($_.finishTime)) - $([datetime]::Parse($_.startTime))).TotalSeconds
+                            $Blb.Duration = "$([Math]::Round($Duration, 2)) s"
+
+                        }
+                        $Blb
+                }
+                Write-Host
+                Write-Host "Branches build status" -f Green
+                $LBuildStats | Format-Table
+            }
+            ################################ Status report ######################################
+            3 {
+                $BrLastBuild = (GetAPIBranches -APPName $Application -APIToken $APIToken).lastbuild | select sourceBranch, id, status, result, startTime, finishTime
+        
+                $LBuildStats = $BrLastBuild|foreach {
+                        $Blb = "" | select BranchName, Status, result, Duration, LogLink
+                        $Blb.BranchName = $_.sourceBranch
+                        $Blb.LogLink =  (GetBuildLogs -APPName $Application -APIToken $APIToken -BuildID $_.id).uri
+                        $Blb.Status = $_.status
+                        $Blb.Result = $_.result
+                        if ($Blb.Status -eq "completed")
+                        {
+                            $Duration = ($([datetime]::Parse($_.finishTime)) - $([datetime]::Parse($_.startTime))).TotalSeconds
+                            $Blb.Duration = "$([Math]::Round($Duration, 2)) s"
+                        }
+                        $Blb
+                }
+                Write-Host
+                Write-Host "Build report" -f Green
+                $LBuildStats | Format-Table
+            }
+            ############################### Save status report to html ###################################
+            4 {
+                $BrLastBuild = (GetAPIBranches -APPName $Application -APIToken $APIToken).lastbuild | select sourceBranch, id, status, result, startTime, finishTime
+        
+                $LBuildStats = $BrLastBuild|foreach {
+                        $Blb = "" | select BranchName, Status, Result, Duration, LogLink
+                        $Blb.BranchName = $_.sourceBranch
+                        $Blb.LogLink =  (GetBuildLogs -APPName $Application -APIToken $APIToken -BuildID $_.id).uri
+                        $Blb.Status = $_.status
+                        $Blb.Result = $_.result
+                        if ($Blb.Status -eq "completed")
+                        {
+                            $Duration = ($([datetime]::Parse($_.finishTime)) - $([datetime]::Parse($_.startTime))).TotalSeconds
+                            $Blb.Duration = "$([Math]::Round($Duration, 2)) s"
+                        }
+                        $Blb
+                }
+                Write-Host
+                Write-Host "Save report to HTML" -f Green
+
+                $Header = @"
+<style>
+TABLE {border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse;} 
+TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
+TH {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
+</style>
+"@
+
+                [System.Web.HttpUtility]::HtmlDecode($($LBuildStats | select -property @{N='Branch name';E={$_.BranchName}},`
+                  @{N='Build status';E={$_.Result}}, @{N='Duration';E={$_.Duration}}, @{N='Link to build logs';E={"<a href='$($_.LogLink)'>Link</a>"}}`
+                  |  ConvertTo-Html -As Table -Head $Header )) |Out-File -FilePath "$($PSScriptRoot)\$($Application)_BuildReport.html"
+                Write-Host "Report saved to $($PSScriptRoot)\$($Application)_BuildReport.html" -f Green
+            }
+            #################################### Exiting script #############################
+            5 {
+                Write-Host
+                Write-Host "Exiting script, bye!" -f Green
+                #exit
+            }
+           # DEFAULT {
+           #     "Script will be stopped"
+           #     exit
+           # }
+        }
+    }
+}
+else
+{
+    Write-Host "Error in using API Token" -ForegroundColor Red
+    Write-Host "Script will be stopped"
+    exit
+}
